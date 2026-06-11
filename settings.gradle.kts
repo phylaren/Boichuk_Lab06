2abe0b79b930a4723fa33dab1f646de889378114
phylaren <julboichuk@gmail.com>
2026-06-11 15:50:21 +0300

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "Boichuk_Lab06"
---
+++ src/test/java/sixth/practice/boichuk_lab06/BoichukLab06ApplicationTests.java
@@ -0,0 +1,13 @@
+package sixth.practice.boichuk_lab06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoichukLab06ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
